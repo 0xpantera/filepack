bbd5213d96f61740fefc7d66507db1f625a659e5
0xpantera <0xpantera@proton.me>
2024-08-21 13:44:46 -0600

    add dependencies to test module

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,18 @@
 module Main (main) where
 
+import FilePack
+import qualified Data.ByteString as BS
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented."
+
+testPackFile :: ByteString
+testPackFile =
+    packFiles sampleFilePack
+
+testUnpackFile :: Either String FilePack
+testUnpackFile = unpackFiles testPackFile
+
+testRoundTrip :: FilePack -> Bool
+testRoundTrip pack =
+    (Right pack) == (unpackFiles $ packFiles pack)
